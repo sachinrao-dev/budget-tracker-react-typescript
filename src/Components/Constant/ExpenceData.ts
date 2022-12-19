2742c7d871e4558d9073997270456c96d1c2d623
sachinrao2811 <sachinrao9450@gmail.com>
2022-12-19 17:45:52 +0530

    implment in expenseData info

--- src/Components/Constant/ExpenceData.ts
+++ src/Components/Constant/ExpenceData.ts
@@ -1,39 +1,65 @@
 interface AllExpense {
-    food:[{
-        expenseCategory:string,
-        date: string,
-        amount: number
-    },],
-  rent: [{
-    expenseCategory:string,
-    date: string,
-     amount: number
-    }],
-    travel:[{
-        expenseCategory:string,
-        date: string,
-        amount: number
-    }],
-    fun:[{
-        expenseCategory:string,
-        date: string,
-        amount: number
-    }],
-}
-
-const getDataFromApi = () =>{
-    const dataPromise= new Promise((resolve)=>{
-        const expense:AllExpense[] = [
-            food: [{
-                expenseCategory:"Food",
-                date:"6",
-                amount: 78
-            },],
-
-        ]
+  food: [
+    {
+      expenseCategory: string;
+      date: string;
+      amount: number;
     }
-    )
+  ];
+  rent: [
+    {
+      expenseCategory: string;
+      date: string;
+      amount: number;
+    }
+  ];
+  travel: [
+    {
+      expenseCategory: string;
+      date: string;
+      amount: number;
+    }
+  ];
+  fun: [
+    {
+      expenseCategory: string;
+      date: string;
+      amount: number;
+    }
+  ];
 }
 
+const getDataFromApi = () => {
+  const dataPromise = new Promise((resolve) => {
+    const expense: AllExpense[] = [
+      {
+        food: [
+          {
+            expenseCategory: "Food",
+            date: "222",
+            amount: 122
+          },
+        ],
+        rent:[{
+          expenseCategory: "Rent",
+          date: "22",
+          amount:33,
+        },],
+        travel:[{
+          expenseCategory: "Fun",
+          date: "2333",
+          amount: 33,
+        },],
+        fun:[{
+          expenseCategory: "Fun",
+          date: "23",
+          amount: 22,
+        },]
+      },
+    ];
+    resolve(expense);
+  });
+  return dataPromise;
+};
 
 export default getDataFromApi;
