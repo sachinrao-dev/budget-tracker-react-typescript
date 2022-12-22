1c805351757a39ddc5169d23f25605134a32ff84
sachinrao2811 <sachinrao9450@gmail.com>
2022-12-22 11:21:17 +0530

    implement in expense info

--- src/Components/Constant/ExpenseData.ts
+++ src/Components/Constant/ExpenseData.ts
@@ -1,42 +1,65 @@
 import { createContext } from "react";
+import AllExpense from "../Expense/AllExpense";
 
 interface AllExpense {
-  food: [
-    {
+  food: {
       expenseCategory: string,
       date: string,
       amount: number,
-    }
-  ],
-  rent: [
-    {
+    }[],
+  rent:{
       expenseCategory: string;
       date: string;
       amount: number;
-    }
-  ],
-  travel: [
-    {
+    }[],
+  travel:{
       expenseCategory: string;
       date: string;
       amount: number;
-    }
-  ],
-  fun: [
-    {
+    }[],
+  fun:{
       expenseCategory: string;
       date: string;
       amount: number;
-    }
-  ],
+    }[],
 }
 
 const expense: AllExpense = {
-  food: [
+  food:[
     {
       expenseCategory: "Food",
-      date: "2022-12-21",
-      amount: 122
+      date: "22-12-2022",
+      amount: 172
+    },
+    {
+      expenseCategory: "Food",
+      date: "21-12-2022",
+      amount: 162
+    },
+    {
+      expenseCategory: "Food",
+      date: "18-12-2022",
+      amount: 162
+    },{
+      expenseCategory: "Food",
+      date: "20-12-2022",
+      amount: 152
+    },{
+      expenseCategory: "Food",
+      date: "19-12-2022",
+      amount: 142
+    },{
+      expenseCategory: "Food",
+      date: "17-12-2022",
+      amount: 1224
+    },{
+      expenseCategory: "Food",
+      date: "12-12-2022",
+      amount: 1223
+    },{
+      expenseCategory: "Food",
+      date: "10-10-2022",
+      amount: 123
     },
   ],
   rent:[{
--- src/Components/FilterTabBar.tsx
+++ src/Components/FilterTabBar.tsx
@@ -4,16 +4,25 @@ import useStyle from "./Style/FilterTabBarStyle";
 import { expenseContext } from "./Constant/ExpenseData";
 
 function FilterTabBar() {
-  // const expenseData = useContext<any>(expenseContext);
-  // const ClickHandler = ( filter:string )=>{
-  // expenseData["food"].filter((food)=> {
-  //   return new Date(food.date) 
-  // }})
-  // };
+  const expenseData = useContext<any>(expenseContext);
+  // console.log(new Date(expenseData["food"][0].date).getUTCDate());
+  const  ClickHandler = () =>{
+    // console.log(expenseData, "this is expenseData");
+
+    const date = new Date();
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const currentDate = `${day}-${month}-${year}`;
+    console.log(currentDate, "current date");
+
+    
+  };
   const classes = useStyle();
   return (
     <div className={classes.TabBarContainer }>
-      <NavLink className={classes.nav} to="sasas">Today</NavLink >
+      <NavLink  onClick={() => ClickHandler()} className={classes.nav} to="sasas">Today</NavLink >
       <NavLink className={classes.nav} to="ss\r">Week</NavLink >
       <NavLink className={classes.nav} to="se">Years</NavLink >
       <NavLink className={classes.nav} to="ff">Last 5 years</NavLink >
